51d79f9b6c8dafa4dc0bf845724d1b9ab9e055a8
Johnson Joseph <jhonsonj88@gmail.com>
2019-09-01 02:51:11 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,32 @@ function preventRefreshOnSubmit(){
       event.preventDefault()
     })
 }
-
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi(){
+  let list = document.createElement('li')
+
+  list.innerHTML = retrieveEmployeeInformation()
+
+  return document.querySelector('.employee-list').appendChild(list);
+}
+
+function addNewLiOnClick(){
+  let newList = document.querySelector('input')
+
+  newList.addEventListener("click", addNewElementAsLi());
+
+  newList.value = ""
+}
+
+function clearEmployeeListOnLinkClick(){
+  // query for input type a & list
+  let button = document.querySelector('a')
+  let list = document.querySelector('ul')
+
+  button.addEventListener('click', function(e){ list.innerHTML = ''})
+}
